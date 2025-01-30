2ca731cf0bef4fc7af007a4db9865ab066b64871
younas <younas@localhost.localdomain>
2025-01-30 08:44:52 +0000

    Added the final add Job and edit feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,57 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NoFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage'
+import Edit from "./pages/Edit";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<Homepages />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
 
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application-json',
+
+      },
+      body: JSON.stringify(newJob)
+    }) ;
+
+    return
+  }
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+    
+  }
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application-json',
+
+      },
+      body: JSON.stringify(job)
+    });
+
+    return
+  }
+
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Homepages />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader}  />
+        <Route path='/edit-job/:id' element={<Edit updateJobSubmit={updateJob}/>} loader={jobLoader} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 };
 
